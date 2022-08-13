b2356da5df7063491365bad8f56ce335496be667
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-08-13 22:19:48 +0600

    dom node and add element

--- js/dom.js
+++ js/dom.js
@@ -54,3 +54,72 @@ console.log(document.getElementsByClassName('elements-container')[0].innerText);
 const objectContainer = document.getElementById('object-container');
 objectContainer.classList.add('object-border');
 objectContainer.classList.remove('object-bg');
+
+
+/* DOM node */
+
+const nodeContainer = document.getElementById('node-container');
+console.log(nodeContainer.childNodes);
+console.log(nodeContainer.firstChild);
+console.log(nodeContainer.childNodes[5]);
+console.log(nodeContainer.childNodes[5].childNodes[5]);
+console.log(nodeContainer.childNodes[5].childNodes[5].nextSibling);
+
+//add element
+//select where to add
+const nodeUl = document.querySelector('#node-container ul');
+console.log(nodeUl);
+//what to be added
+const newLi = document.createElement('li');
+newLi.innerText = 'appendChild';
+console.log(newLi);
+//add the child
+nodeUl.appendChild(newLi);
+
+console.log(nodeUl.parentNode);
+
+
+/* section add */
+
+const mainContainer = document.getElementById('main-container');
+
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'Create HTML elements';
+section.appendChild(h1);
+const p = document.createElement('p');
+p.innerText = 'The appendChild() method of the Node interface adds a node to the end of the list of children of a specified parent node. If the given child is a reference to an existing node in the document, appendChild() moves it from its current position to the new position.'
+section.appendChild(p);
+
+const ul = document.createElement('ul');
+
+const li1 = document.createElement('li');
+li1.innerText = 'select where to add';
+ul.appendChild(li1);
+
+const li2 = document.createElement('li');
+li2.innerText = 'what to be added';
+ul.appendChild(li2);
+
+const li3 = document.createElement('li');
+li3.innerText = 'add the child';
+ul.appendChild(li3);
+
+section.appendChild(ul);
+
+mainContainer.appendChild(section);
+
+//set innerHTML directly
+
+const sectionDirect = document.createElement('section');
+sectionDirect.innerHTML = `
+<h1>Create HTML elements directly</h1>
+<p>Here we can easily create a html element. we can use innerHTML for this and add elements one by one</p>
+<ul>
+    <li>create a section</li>
+    <li>add html elements into innerHTML</li>
+    <li>add the child</li>
+</ul>
+`
+
+mainContainer.appendChild(sectionDirect);
